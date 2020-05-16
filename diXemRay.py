ba14cbfb1c1ed44fbb8d9ee9e7269d4094b18f4e
phuong <phuong@boo.com>
2020-05-16 11:09:23 +0700

    PhuongNT74: Add feature auto taking off
    Signed-off-by: phuong <phuong@boo.com>

--- diXemRay.py
+++ diXemRay.py
@@ -10,7 +10,16 @@ def setLichTrinh(doCao, diaDiem):
 	print("Do cao la: ", doCao)
 	print("Dia diem la: ", diaDiem)
 
-print(" Hi phuong")
+def autoTakingOff():
+	from time import sleep
+	timeTakingOff = 10
+	while timeTakingOff > 0:
+		print("Flycam is taking off: ", timeTakingOff)
+		sleep(1)
+		timeTakingOff -= 1
+
+print("Flycam is running ...")
 setLichTrinh(1000, "Tan Nghia")
+autoTakingOff()
 
 
