448cb5400d06a66955aac770aceb7133185afe6c
phuong <phuong@boo.com>
2020-05-23 20:22:47 +0700

    Delete all add

--- banana.c
+++ banana.c
@@ -1,7 +1,12 @@
 //Viec can lam
 /*
+<<<<<<< HEAD
 1. Ngu day, ve sinh ca nhan, an sang
 2. Coffee, pha it duong, them da
+=======
+1. An sang
+2. Coffee
+>>>>>>> parent of 9066408... phuong: xoa commit ke truoc
 3. Learn git
 4. Learn python
 5. Learn poky
