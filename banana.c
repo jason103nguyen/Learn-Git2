c0e60fead75b2c65fc37b7af022aba9d92579128
phuong <phuong@boo.com>
2020-05-23 20:30:35 +0700

    revert hoa

--- banana.c
+++ banana.c
@@ -3,8 +3,13 @@
 1. Ngu day, ve sinh ca nhan, an sang
 2. Coffee, pha it duong, them da
 3. Learn toan
+<<<<<<< HEAD
 4. Learn ly
 5. Learn hoa
+=======
+4. Learn python
+5. Learn poky
+>>>>>>> parent of 947289e... add ly
 6. Giat do
 7. Ngu
 */
