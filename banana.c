79aa5fe5ececd4b4306b6681a9d6accea08d12d5
phuong <phuong@boo.com>
2020-05-23 20:27:06 +0700

    add toan

--- banana.c
+++ banana.c
@@ -1,14 +1,8 @@
 //Viec can lam
 /*
-<<<<<<< HEAD
-1. An sang
-2. Coffee
-3. Learn toan
-=======
 1. Ngu day, ve sinh ca nhan, an sang
 2. Coffee, pha it duong, them da
-3. Learn git
->>>>>>> parent of 448cb54... Delete all add
+3. Learn toan
 4. Learn python
 5. Learn poky
 6. Giat do
