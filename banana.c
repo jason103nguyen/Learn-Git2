85340104ffef6e1be3145af0666a8b6f180d517a
phuong <phuong@boo.com>
2020-05-23 20:26:21 +0700

    phuong: trai ot

--- banana.c
+++ banana.c
@@ -1,8 +1,14 @@
 //Viec can lam
 /*
+<<<<<<< HEAD
 1. An sang
 2. Coffee
 3. Learn toan
+=======
+1. Ngu day, ve sinh ca nhan, an sang
+2. Coffee, pha it duong, them da
+3. Learn git
+>>>>>>> parent of 448cb54... Delete all add
 4. Learn python
 5. Learn poky
 6. Giat do
